6b63b099288530f5a1e69a19d979d78954ec1ab9
Crillibert <70443967+Crillibert@users.noreply.github.com>
2025-03-04 17:18:07 -0600

    Addicion del lector de archivos
    De momento solo esta para leer .txt
    Cualquier cambio deberia de poder arreglarse facil

---
+++ LectorDeArchivos.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+public class LectorDeArchivos {
+    //Se crea una lista de listas, donde cada lista son los tokens analizados
+    private String nombreArchivo;
+    public LectorDeArchivos(String pNombre){
+        this.nombreArchivo = pNombre;
+    }
+    public List<List<String>> LeerArchivo(){
+        List<List<String>> ListaFinal = new ArrayList<List<String>>();
+        try {
+            File Archivo = new File(nombreArchivo);
+            Scanner Lector = new Scanner(Archivo);
+            while (Lector.hasNextLine()){
+                String Linea = Lector.nextLine();
+                AnLexico scanner  =  new AnLexico(Linea); 
+                List<String> Tokens = scanner.AnalizadorCadena();
+                ListaFinal.add(Tokens);
+            }
+            Lector.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error, no se encontro el archivo.");
+            e.printStackTrace();
+           /*  List<String> listaError = new ArrayList<>();
+            listaError.add("ø");
+            ListaFinal.add(listaError);*/
+        }
+        return ListaFinal;
+    }
+    //Al implementar el front end, esta parte se va
+    //Se puede usar de ejemplo de como funcionara
+    public static void main(String[] args) {
+        LectorDeArchivos scanner = new LectorDeArchivos("filename.txt");
+        List<List<String>> Tokens = scanner.LeerArchivo();
+        for (List<String> s : Tokens){
+            if(!s.isEmpty()){
+                System.out.println(s.get(0));
+                for(int i = 1; i < s.size(); i++){
+                    System.out.println(s.get(i));
+                }
+            }
+            System.out.println("________________________");
+        }
+    }
+    
+}
