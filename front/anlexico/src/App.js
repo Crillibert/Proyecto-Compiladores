09965635983da6164172cafb0e3ff58dbd97fe07
Blasko <04dani.ruano@gmail.com>
2025-03-17 19:43:46 -0600

    Cambio front-end

--- front/anlexico/src/App.js
+++ front/anlexico/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import SeleccionarArchivo from './components/SeleccionarArchivo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Analizador Léxico</h1> {/* Título agregado */}
-        <textarea
-          placeholder="Ingresa tu código aquí..."
-          style={{ width: '50%', height: '150px', margin: '20px', padding: '10px', fontSize: '16px' }}
-        />
         <button
           onClick={() => alert("Analizando...")}
           style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}
         >
           Analizar
         </button>
+        <img src={logo} className="App-logo" alt="logo" />
+          <SeleccionarArchivo/>
+  
       </header>
     </div>
   );
